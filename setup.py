0fbcba1085a3ea936605a11d489af6b25a9de8ae
wulan17 <wulan17@nusantararom.org>
2024-03-21 13:58:03 +0700

    Bump version to v1.2.6
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="TgCrypto-pyrofork",
-    version="1.2.5",
+    version="1.2.6",
     description="Fast and Portable Cryptography Extension Library for Pyrofork",
     long_description=readme,
     long_description_content_type="text/markdown",
